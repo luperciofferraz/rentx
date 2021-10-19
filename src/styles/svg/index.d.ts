fd89d4eb7580e8427f5236714ea327dd79ad562c
Lupércio <lupercio.ferraz@gmail.com>
2021-10-19 00:48:41 -0300

    Home

---
+++ src/styles/svg/index.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg" {
+
+    import React from 'react';
+    import { SvgProps } from 'react-native-svg';
+
+    const content: React<SvgPrps>
+
+    export default content;
+
+}
