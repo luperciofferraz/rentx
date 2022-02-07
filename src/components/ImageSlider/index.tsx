a759537e789cf3f1091f7234ead8210ecb53bc1c
Lupércio <lupercio.ferraz@gmail.com>
2022-02-07 06:53:05 -0300

    Cars Silder

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { FlatList, ViewToken } from 'react-native';
 import {
 
     Container,
@@ -14,31 +15,60 @@ interface Props {
     imagesUrl: string[];
 }
 
+interface ChangeImageProps {
+
+    viewableItems: ViewToken[];
+    changed: ViewToken[];
+
+}
+
 export function ImageSlider({ imagesUrl }:Props) {
+
+    const [imageIndex, setImageIndex] = useState(0);
+
+    const indexChanged = useRef((info: ChangeImageProps) => {
+        const index = info.viewableItems[0].index!;
+        setImageIndex(index);
+    });
 
     return (
 
         <Container>
 
             <ImageIndexes>
+               
+                {
+                    imagesUrl.map((_,index) => (
 
-                <ImageIndex active={true} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
-                <ImageIndex active={false} />
+                        <ImageIndex 
+                            key={String(index)}
+                            active={index === imageIndex} 
+                        />
+
+                    ))    
+                }
 
             </ImageIndexes>
 
-            <CarImageWrapper>
+            <FlatList
+                data={imagesUrl}
+                keyExtractor={key => key}
+                renderItem={({ item }) => (
 
-                <CarImage 
+                    <CarImageWrapper>
+                        <CarImage 
 
-                    source={{ uri: imagesUrl[0]}}
-                    resizeMode="contain"
-                
-                />
+                            source={{ uri: item }}
+                            resizeMode="contain"
+                        
+                        />
+                    </CarImageWrapper>
 
-            </CarImageWrapper>
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                onViewableItemsChanged={indexChanged.current}
+            />
 
         </Container>
 
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -82,7 +82,7 @@ export function Home() {
                     />
 
                     <TotalCars>
-                        Total de 12 carros
+                        Total de {cars.length} carros
                     </TotalCars>
                 </HeaderContent>
 
